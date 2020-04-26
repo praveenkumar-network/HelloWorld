db696063db108b98bcb73d6da1b15418f6703762
praveenkumar-network <64351232+praveenkumar-network@users.noreply.github.com>
2020-04-26 15:23:40 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@
 public class Hello {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World World");
+		for (int i = 0; i < 2; i++) {
+			System.out.println("Hello World");
+		}
 	}
-
 }
